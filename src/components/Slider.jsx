44d24e9fc40a86c98a69a48c1208196701ccb75e
Sujoy Giri <girisujoy630@gmail.com>
2023-03-23 21:21:42 +0530

    added download and share functionlity

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -2,28 +2,37 @@ import React, { useState } from 'react';
 import {
   Grid,
   Container,
-  Box
+  Box,
+  Stack,
+  Paper
 } from '@mui/material';
 
-import { ArrowCircleRightRounded, ArrowCircleLeftRounded } from '@mui/icons-material';
+import {
+  ArrowCircleRightRounded,
+  ArrowCircleLeftRounded,
+  FavoriteBorderRounded,
+  ShareRounded,
+  DownloadRounded
+} from '@mui/icons-material';
 
 const Slider = () => {
 
   const [imageURL, setImageURL] = useState('https://i.waifu.pics/CWZ2Q2V.jpg');
+  
 
   return (
     <React.Fragment>
       <Container disableGutters={true} >
-        <Grid container sx={{ height: '90vh' }} >
+        <Grid container sx={{ height: '82vh' }} >
           <Grid item xs={2} sm={2} md={2} lg={3} xl={3} style={{ width: 'inherit', height: 'inherit' }}>
             <Box sx={{ display: 'flex', width: 'inherit', height: 'inherit', justifyContent: 'center', alignItems: 'center' }}>
               <ArrowCircleLeftRounded />
             </Box>
           </Grid>
           <Grid item xs={8} sm={8} md={8} lg={6} xl={6} style={{ width: 'inherit', height: 'inherit' }}>
-            <div style={{ width: 'inherit', height: 'inherit' }}>
+            <Box sx={{ width: 'inherit', height: 'inherit' }} >
               <img src={imageURL} loading='lazy' style={{ width: 'inherit', height: 'inherit' }} />
-            </div>
+            </Box>
           </Grid>
           <Grid item xs={2} sm={2} md={2} lg={3} xl={3} style={{ width: 'inherit', height: 'inherit' }}>
             <Box sx={{ display: 'flex', width: 'inherit', height: 'inherit', justifyContent: 'center', alignItems: 'center' }}>
@@ -31,6 +40,13 @@ const Slider = () => {
             </Box>
           </Grid>
         </Grid>
+        <Box sx={{display:'flex',justifyContent:'center',alignItems:'center',marginTop:'2%'}} >
+          <Stack direction="row" spacing={10}>
+            <Paper elevation={3}><DownloadRounded /></Paper>
+            <Paper elevation={3}><FavoriteBorderRounded /></Paper>
+            <Paper elevation={3}><ShareRounded /></Paper>
+          </Stack>
+        </Box>
       </Container>
     </React.Fragment>
   );
